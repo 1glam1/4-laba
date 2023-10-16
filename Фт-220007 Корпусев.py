ee6a6a86952b12606e55f3416adb12d161e2711e
1glam1 <102163352+1glam1@users.noreply.github.com>
2023-10-16 13:10:16 +0500

    Добавлен диалог понятный пользователю(при введении неверного числа).

--- Фт-220007 Корпусев.py
+++ Фт-220007 Корпусев.py
@@ -6,19 +6,21 @@ class Numbers():
     def add_numbers(self):
         rub = ''
         n = ''
-        if 1<=int(self.number)<=100000:
-            if int(self.number)%10==1:
-                rub ='рубль'
-            elif int(self.number)%10==2 or int(self.number)%10==3 or int(self.number)%10==4:
-                rub ='рубля'
-            elif (int(self.number)%10==5 or int(self.number)%10==6 
-            or int(self.number)%10==7 or int(self.number)%10==8 
-            or int(self.number)%10==9 or int(self.number)%10==0):
-                rub ='рублей'
-            n = num2words(self.number, lang='ru')+' '+rub
-            print(n.capitalize())
-        else:
-            raise ValueError
-        
+        while True:
+            if 1<=int(self.number)<=100000:
+                if int(self.number)%10==1:
+                    rub ='рубль'
+                elif int(self.number)%10==2 or int(self.number)%10==3 or int(self.number)%10==4:
+                    rub ='рубля'
+                elif (int(self.number)%10==5 or int(self.number)%10==6 
+                or int(self.number)%10==7 or int(self.number)%10==8 
+                or int(self.number)%10==9 or int(self.number)%10==0):
+                    rub ='рублей'
+                n = num2words(self.number, lang='ru')+' '+rub
+                print(n.capitalize())
+                break
+            else:
+                self.number = input('Вы ввели неверное число. Введите число, входящее в диапозон от 1 до 100000: ')
+                continue  
 a = Numbers()
 a.add_numbers()
